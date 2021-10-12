163ce35ec3785def8973671c6972a58492b5ba8c
Rubem Silva <rubekx@gmail.com>
2021-10-12 16:55:12 -0300

    add script to connect contianers to other network

---
+++ connect-containers-network.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#REDES
+rede_a='rede-a'
+rede_b='rede-b'
+
+#CONAINERS
+container_a1='app-a'
+container_a2='mysql-a'
+container_a3='phpmyadmin-a'
+container_b1='app-b'
+container_b2='mysql-b'
+container_b3='phpmyadmin-b'
+
+echo 'Conectando conainters A com a rede B'
+sudo docker network connect rede_b container_a1
+sudo docker network connect rede_b container_a2
+sudo docker network connect rede_b container_a3
+echo 'Conectando conainters B com a rede a'
+sudo docker network connect rede_a container_b1
+sudo docker network connect rede_a container_b2
+sudo docker network connect rede_a container_b3
