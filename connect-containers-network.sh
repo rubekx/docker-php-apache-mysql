c4cebf28be72a9dc42174be2eb6c7740207ef146
Rubem <rubekx@gmail.com>
2021-10-12 17:00:37 -0300

    fix variable syntax

--- connect-containers-network.sh
+++ connect-containers-network.sh
@@ -13,10 +13,10 @@ container_b2='mysql-b'
 container_b3='phpmyadmin-b'
 
 echo 'Conectando conainters A com a rede B'
-sudo docker network connect rede_b container_a1
-sudo docker network connect rede_b container_a2
-sudo docker network connect rede_b container_a3
+sudo docker network connect $rede_b $container_a1
+sudo docker network connect $rede_b $container_a2
+sudo docker network connect $rede_b $container_a3
 echo 'Conectando conainters B com a rede a'
-sudo docker network connect rede_a container_b1
-sudo docker network connect rede_a container_b2
-sudo docker network connect rede_a container_b3
+sudo docker network connect $rede_a $container_b1
+sudo docker network connect $rede_a $container_b2
+sudo docker network connect $rede_a $container_b3
